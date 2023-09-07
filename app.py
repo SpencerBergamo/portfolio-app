3f41a9b219cc3f888e0550b1c743d7ae9bbb59b4
spencerBergamo@gmail.com <spencerbergamo@gmail.com>
2023-09-06 22:22:50 -0500

    switching locally

--- app.py
+++ app.py
@@ -29,8 +29,8 @@ def get_database_url() -> str:
 
 
 SLACK_WEBHOOK = 'https://hooks.slack.com/services/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'
-app.config['SQLALCHEMY_DATABASE_URI'] = get_database_url()
-# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgresql:///portfolio')
+# app.config['SQLALCHEMY_DATABASE_URI'] = get_database_url()
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgresql:///portfolio')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'myportfolio')
@@ -57,9 +57,11 @@ def do_logout():
 
 # User first route
 @app.route('/')
-def route():
+def root():
 
     return redirect('/homepage')
+
+
 
 @app.route('/homepage')
 def homepage():
