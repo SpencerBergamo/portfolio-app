cbe432af5a8a164d5ae7bb618fae3656f2e661a9
spencerBergamo@gmail.com <spencerbergamo@gmail.com>
2023-09-07 11:37:16 -0500

    added a few changes

--- app.py
+++ app.py
@@ -29,8 +29,8 @@ def get_database_url() -> str:
 
 
 SLACK_WEBHOOK = 'https://hooks.slack.com/services/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'
-# app.config['SQLALCHEMY_DATABASE_URI'] = get_database_url()
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgresql:///portfolio')
+app.config['SQLALCHEMY_DATABASE_URI'] = get_database_url()
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgresql:///portfolio')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'myportfolio')
